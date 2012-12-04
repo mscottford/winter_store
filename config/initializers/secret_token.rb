2637b42aac049c7468b3bf22e5d1a0584ad7ba6b
M. Scott Ford <scott@mscottford.com>
2012-12-04 16:46:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WinterStore::Application.config.secret_token = 'a47850091f50d732b8112945b3392048267c0ab0d3fd42452f099cf70ecea7b3b6a1207a1f33956ff420fcda73ea1aa8987099b11c98becfccdb3229aeac1b1a'
